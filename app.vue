ee33c35dbfec8fe304b9191f7710ca7c25afa91a
Joachim <kern_jo@web.de>
2021-11-09 19:28:36 +0100

    Apply basic transitions and Stylings

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <main class="min-h-screen w-screen overflow-x-hidden bg-">
+  <main 
+    class="min-h-screen w-screen overflow-x-hidden dark:bg-dark-800 bg-light-300"
+    wd:transition="all duration-300"
+  >
     <NuxtPage />
   </main>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
     <div 
-        md="container mx-auto" 
-        p="x-4 y-2"
-        text="center"
+        wd:md="container mx-auto" 
+        wd:p="x-4 y-2"
+        wd:text="center"
     >
-        <h1 class="font-monofett">My personal website!</h1>
+        <h1 
+            wd:font="monofett"
+            wd:text="3xl dark:white"
+            wd:transition="all duration-300"
+        >
+            My personal website!
+        </h1>
     </div>
 </template>
 
